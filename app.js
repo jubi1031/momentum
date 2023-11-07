e193712206055383510ae662459efeec8621ebdc
jubi <joobe1031@gmail.com>
2023-11-07 17:35:05 +0900

    계산

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const calcultor = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  div: function (a, b) {
+    console.log(a / b);
+  },
+  multi: function (a, b) {
+    console.log(a * b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
 
+calcultor.add(1, 2);
+calcultor.minus(1, 2);
+calcultor.div(1, 2);
+calcultor.multi(1, 2);
+calcultor.power(1, 2);
